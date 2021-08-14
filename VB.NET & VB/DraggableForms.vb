77e10be6fd3239312a190bdeda36a8bb878b27b9
Byron <53088136+byronbytes@users.noreply.github.com>
2021-08-14 18:36:41 -0400

    DraggableForms

---
+++ VB.NET & VB/DraggableForms.vb
@@ -0,0 +1,19 @@
+## Replace the Panel1_MouseDown with your own class, NOTE: Must be a MouseDown event.
+    
+    Public Const WM_NCLBUTTONDOWN As Integer = &HA1
+    Public Const HT_CAPTION As Integer = &H2
+
+    <DllImportAttribute("user32.dll")>
+    Public Shared Function SendMessage(ByVal hWnd As IntPtr, ByVal Msg As Integer, ByVal wParam As Integer, ByVal lParam As Integer) As Integer
+    End Function
+
+    <DllImportAttribute("user32.dll")>
+    Public Shared Function ReleaseCapture() As Boolean
+    End Function
+
+    Private Sub Panel1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseDown
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            ReleaseCapture()
+            SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0)
+        End If
+    End Sub
