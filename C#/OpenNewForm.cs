69c1eae2b15e1b3bb2a93fb00b9b94ea815f14f1
Byron <53088136+byronbytes@users.noreply.github.com>
2021-08-14 18:31:56 -0400

    Create OpenNewForm.cs

---
+++ C#/OpenNewForm.cs
@@ -0,0 +1,7 @@
+//Note: FullFormName is the name of the form that is defined (aka Form1 or whatever you named it to) CustomFormName can be anything.
+
+ FullFormName CustomFormName = new FullFormName(); // Initializes the form.
+            CustomFormName.Show(); // Shows the form.
+            
+            
+      
