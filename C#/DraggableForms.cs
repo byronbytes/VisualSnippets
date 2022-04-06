ffb06e8d406cb86bfe44df29b53dcdfc4c3d770e
Matty <53088136+byronbytes@users.noreply.github.com>
2022-04-06 08:45:51 -0400

    Update DraggableForms.cs

--- C#/DraggableForms.cs
+++ C#/DraggableForms.cs
@@ -1,25 +1,20 @@
-// NOT MY CODE. Originally found on StackOverflow (Dont know where anymore)
-// Imports Required: System.Runtime.InteropServices
-//* Replace the panel2_MouseDown with your own class, NOTE: Must be a MouseDown event.
-
-
-using System.Runtime.InteropServices;
+using System.Runtime.InteropServices; // Import needed for this to work
 
  public const int WM_NCLBUTTONDOWN = 0xA1;
         public const int HT_CAPTION = 0x2;
 
+        [System.Runtime.InteropServices.DllImport("user32.dll")] 
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam); // Sends message to the 
         [System.Runtime.InteropServices.DllImport("user32.dll")]
-        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
-        public static extern bool ReleaseCapture();
+        public static extern bool ReleaseCapture(); // Gets when you hold form down
 
 
-        private void panel2_MouseDown*(object sender, MouseEventArgs e)
+        private void panel2_MouseDown(object sender, MouseEventArgs e) // The class must have a MouseEventArgs event for it to work.
         {
-            if (e.Button == MouseButtons.Left)
+            if (e.Button == MouseButtons.Left) // If the mouse button is left
             {
-                ReleaseCapture();
-                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+                ReleaseCapture(); // Gets when you hold the form down
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0); // Sends the message that it's draggable
             }
         }
         
