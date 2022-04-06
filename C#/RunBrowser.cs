89fc5c3f634dcb1cccd06997db93514ffba28d7e
Matty <53088136+byronbytes@users.noreply.github.com>
2022-04-06 08:49:25 -0400

    Update RunBrowser.cs

--- C#/RunBrowser.cs
+++ C#/RunBrowser.cs
@@ -1,3 +1,3 @@
-  //Note: The weblink can be any website.
+// UseShellExecute makes it run with any native process, for example if you set it to a weblink, it will open up a webbrowser instead of giving you an error.
   
   Process.Start(new ProcessStartInfo { FileName = "https://google.com", UseShellExecute = true });
